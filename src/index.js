607db2f16f500ed80d36ecefcdf9b5a979b04eb1
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2023-12-31 14:07:16 -0300

    ✨ feat: created status route to show info api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 "use strict";
 
-console.log(`API started...`);
+require("dotenv/config");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = Number(process.env.PORT) || 3030;
+
+app.get(`/status`, (_request, response) =>
+  response.status(200).json({
+    API_STATUS: `running`,
+    API_PORT: process.env.PORT || PORT,
+    API_NAME: `foodexplorerapi`,
+  })
+);
+
+app.listen(PORT, () =>
+  console.info(`Server running in http://localhost:${PORT}`)
+);
