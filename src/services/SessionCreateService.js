a03c07aa8d6922b8a1dcda3348feccb68a18d65f
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2024-01-01 05:35:37 +0000

    Jan 1, 2024, 2:35 AM

--- src/services/SessionCreateService.js
+++ src/services/SessionCreateService.js
@@ -1,5 +1,11 @@
 `use strict`;
 
+const knex = require("../database/knex");
+const { compare } = require("bcryptjs");
+const { sign } = require("jsonwebtoken");
+const authConfig = require("../configs/auth");
+const AppError = require("../utils/AppError");
+
 class SessionCreateService {
   constructor(userRepository) {
     this.userRepository = userRepository;
@@ -17,6 +23,13 @@ class SessionCreateService {
     if (!passwordMatched) {
       throw new AppError(`E-mail e/ou senha incorreta.`, 401);
     }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ role: user.role }, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
   }
 }
 
