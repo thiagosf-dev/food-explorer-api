2460cf48baa54acd916d585fd85c2cbbb4135e31
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2024-01-01 05:29:58 +0000

    Jan 1, 2024, 2:29 AM

--- src/services/SessionCreateService.js
+++ src/services/SessionCreateService.js
@@ -1,5 +1,23 @@
-"use strict";
+`use strict`;
 
-class SessionCreateService {}
+class SessionCreateService {
+  constructor(userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async execute({ email, password }) {
+    const user = await knex(`users`).where({ email }).first();
+
+    if (!user) {
+      throw new AppError(`E-mail e/ou senha incorreta.`, 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError(`E-mail e/ou senha incorreta.`, 401);
+    }
+  }
+}
 
 module.exports = SessionCreateService;
