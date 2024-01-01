b8ad7e8c016e7ac462f6094a9fa5b1967c53b688
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2024-01-01 13:27:38 +0000

    Jan 1, 2024, 10:27 AM

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
-const { Router } = require("express");
-const userRoutes = require("./user.routes");
+`use strict`;
+
+const { Router } = require(`express`);
+const userRoutes = require(`./user.routes`);
+const sessionRoutes = require(`./session.routes`);
 
 const routes = Router();
 
-routes.use("/user", userRoutes);
+routes.use(`/user`, userRoutes);
+
+routes.use(`/session`, sessionRoutes);
 
 module.exports = routes;
